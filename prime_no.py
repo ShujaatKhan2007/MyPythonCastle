33847c3afc3cb612bd5fc94bd711a6880b08efbd
ShujaatKhan2007 <Shujaatkhan2007@gmail.com>
2026-02-15 13:17:11 +0530

    Logic to find a given number is a Prime Number

--- prime_no.py
+++ prime_no.py
@@ -3,12 +3,20 @@
 
 no=int(input("Enter a number :-"))
 
-unit_pl= no%10
-once_pl = no/10
-sum= unit_pl+once_pl
+# unit_pl= no%10
+# once_pl = no/10
+# sum= unit_pl+once_pl
 
-if(sum==no):
-    print("perfect number")
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+if(is_prime(no)):
+    print("prime number")
 else:
-    print("not perfect number")
+    print("not prime number")
 
